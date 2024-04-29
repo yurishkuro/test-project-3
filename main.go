e6508e9290dc08d8c5346ce3a5fab2536987848b
Yuri Shkuro <github@ysh.us>
2024-04-28 20:09:33 -0400

    go-lsof
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	cmd := exec.Cmd{
 		Path:   "/bin/bash",
-		Args:   []string{"bash", "-c", "lsof -iTCP -sTCP:LISTEN -P +c0"},
+		Args:   []string{"bash", "-c", "sudo lsof -iTCP -sTCP:LISTEN -P +c0"},
 		Stdout: os.Stderr,
 		Stderr: os.Stderr,
 	}
